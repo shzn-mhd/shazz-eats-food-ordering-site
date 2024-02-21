02e0682b357cfabf26c74c6eb4519bd17c2acd36
shzn-mhd <mohamadshazan221@gmail.com>
2024-02-21 14:34:26 +0530

    json values for foods

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Banner from "./components/Banner";
+import Tiles from "./components/Tiles";
+import Foods from "./components/Foods";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Banner />
+      <Tiles />
+      <Foods />
     </div>
   );
 }
---
+++ src/data/Data.js
@@ -0,0 +1,91 @@
+export const data = [
+    {
+        id: 1,
+        name: 'Double Cheeseburger',
+        category: 'burger',
+        image :
+        '',
+        price: '$$$$',
+    },
+
+    {
+        id: 2,
+        name: 'Bacon Cheeseburger',
+        category: 'burger',
+        image :
+        '',
+        price: '$',
+    },
+
+    {
+        id: 3,
+        name: 'Mushroom Burger',
+        category: 'burger',
+        image :
+        '',
+        price: '$$',
+    },
+
+    {
+        id: 4,
+        name: 'Loaded Burger',
+        category: 'burger',
+        image :
+        '',
+        price: '$$$',
+    },
+
+    {
+        id: 5,
+        name: 'Feta & Spinnach',
+        category: 'pizza',
+        image :
+        '',
+        price: '$$',
+    },
+
+    {
+        id: 6,
+        name: 'Supreme Pizza',
+        category: 'pizza',
+        image :
+        '',
+        price: '$$$$',
+    },
+
+    {
+        id: 7,
+        name: 'Meat Pizza',
+        category: 'pizza',
+        image :
+        '',
+        price: '$$$$',
+    },
+
+    {
+        id: 8,
+        name: 'Cheese Pizza',
+        category: 'pizza',
+        image :
+        '',
+        price: '$',
+    },
+
+    {
+        id: 9,
+        name: 'Mushroom Salad',
+        category: 'salad',
+        image :
+        '',
+        price: '$',
+    },
+
+    {
+        id: 10,
+        name: 'Mix Salad',
+        category: 'salad',
+        image :
+        '',
+        price: '$$$',
+    },
+]
