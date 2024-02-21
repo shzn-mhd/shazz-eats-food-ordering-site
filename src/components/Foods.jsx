367e46790866b68d7371d1323f0c234dc574d058
shzn-mhd <mohamadshazan221@gmail.com>
2024-02-21 16:21:04 +0530

    creating buttons for filter items

--- src/components/Foods.jsx
+++ src/components/Foods.jsx
@@ -2,10 +2,39 @@ import React from 'react'
 import { data } from '../data/Data'
 
 const Foods = () => {
-  console.log(data);
+  // console.log(data);
   return (
     <div>
-      
+      <h1>Top Rated Menu Items</h1>
+
+      {/* Filter Row */}
+      <div>
+        {/* Filter Type */}
+          <div>
+            <p>Filter Type</p>
+            <div>
+              <button>All</button>
+              <button>Burgers</button>
+              <button>Pizza</button>
+              <button>Salads</button>
+              <button>Chicken</button>
+            </div>
+          </div>
+        <div>
+
+          {/* Filter Price */}
+          <div>
+            <p>Filter Price</p>
+            <div>
+              <button>$</button>
+              <button>$$</button>
+              <button>$$$</button>
+              <button>$$$$</button>
+            </div>
+          </div>
+
+        </div>
+      </div>
     </div>
   )
 }
