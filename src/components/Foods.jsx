f4221fa5dcaf622ac922d2a419ecfb53425a1987
shzn-mhd <mohamadshazan221@gmail.com>
2024-02-21 17:22:57 +0530

    stylings for mapped images

--- src/components/Foods.jsx
+++ src/components/Foods.jsx
@@ -41,11 +41,19 @@ const Foods = () => {
       </div>
       
       {/* Display Foods */}
-      <div>
+      <div className='grid grid-cols-2 lg:grid-cols-4 gap-6 pt=4'>
         {foods.map((item, index) => (
-          <divv key={index}>
-            <img src={item.image} alt={`${item.name}-Image`} />
-          </divv>
+          <div key={index} className='border shadow-lg rounded-lg hover:scale-105 duration-300'>
+            <img src={item.image} alt={`${item.name}-Image`} 
+            className='w-full h-[200px] object-cover rounded-t-lg'
+            />
+            <div className='flex justify-between px-2 py-4'>
+              <p className='font-bold'>{item.name}</p>
+              <p>
+                <span className='bg-orange-500 text-white p-1 rounded-full'>{item.price}</span>
+              </p>
+            </div>
+          </div>
         ))}
       </div>
     </div>
