525e57043ce4f7caee44b52a64f4b321a858507f
shzn-mhd <mohamadshazan221@gmail.com>
2024-02-21 17:31:29 +0530

    filter function for category

--- src/components/Foods.jsx
+++ src/components/Foods.jsx
@@ -7,6 +7,25 @@ const Foods = () => {
   const[foods, setFoods] = useState(data)
 
 
+  //filter type foods
+  const filterTypes = (category) => {
+    setFoods(
+      data.filter((item) => {
+        return item.category === category;
+      })
+    );
+  };
+
+  //filter by price
+  const filterPrice = (price) => {
+    setFoods(
+      data.filter((item) => {
+        return item.price ===price;
+      })
+    );
+  };
+
+
   return (
     <div className='max-w-[1640px] m-auto px-4 p-12 py-12'>
       <h1 className='text-orange-600 font-bold text-4xl text-center'>Top Rated Menu Items</h1>
@@ -17,11 +36,35 @@ const Foods = () => {
           <div>
             <p className='font-bold text-gray-700'>Filter Type</p>
             <div className='flex justify-between flex-wrap'>
-              <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>All</button>
-              <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Burgers</button>
-              <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Pizza</button>
-              <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Salads</button>
-              <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Chicken</button>
+              <button 
+                onClick={()=> setFoods(data)} 
+                className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>
+                All
+              </button>
+
+              <button 
+                onClick={()=> filterTypes('burger')} 
+                className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>
+                Burgers
+              </button>
+
+              <button 
+                onClick={()=> filterTypes('pizza')} 
+                className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>
+                Pizza
+              </button>
+
+              <button 
+                onClick={()=> filterTypes('salad')} 
+                className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>
+                Salads
+              </button>
+              
+              <button 
+                onClick={()=> filterTypes('chicken')} 
+                className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>
+                Chicken
+              </button>
             </div>
           </div>
         
